cff7e417d287f1b1e4048ce731fcac611d2ff55f
pokotsun <gorigolila@gmail.com>
2018-04-20 20:15:33 +0900

    Seleniumを用いたテストを作った

--- api/management/commands/selenium_test.py
+++ api/management/commands/selenium_test.py
@@ -1,31 +1,49 @@
 # -- coding: utf-8 --
 from django.core.management.base import BaseCommand, CommandError
 import urllib.request, urllib.error
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-#from api.management.commands import aiueo
-#import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
 
 class Command(BaseCommand):
 
     # python manage.py help count_entryで表示されるメッセージ
-    help = 'カスタムコマンドのテスト'
+    help = 'Seleniumのテスト'
 
-
-
-    # コマンドライン引数を指定する(argparseモジュール)
-    # 今回はblog_idという名前で取得する
-    # def add_arguments(self, parser):
-    #     parser.add_argument('blog_id', nargs='+', type=int)
-    #
     # # コマンドが実行された時に呼ばれるメソッド
     def handle(self, *args, **options):
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
         options.add_argument('--window-size=1280,1024')
+
         driver = webdriver.Chrome(chrome_options=options)
-        driver.get('https://www.google.co.jp/')
-        print(driver.title) #=> Google
-        driver.save_screenshot('test.png')
+        driver.get('https://edragon-syllabus.jimu.kyutech.ac.jp/guest/syllabuses') # ページを開く
+        print(driver.title) #=> 九州工業大学シラバス
+        faculty_select = driver.find_element_by_id('belong_children_faculty') # エレメントを取得する
+        faculty_select_element = Select(faculty_select)
+        faculty_select_element.select_by_value('278')
+    
+        driver.find_element_by_class_name('js-simple-search-btn').click() # 検索ボタンをクリック
+
+
+        sleep(5)
+        syllubuse_links = driver.find_elements_by_class_name('js-syllabus-show-link')
+
+        #print(syllubuse_links)
+        for (i, link) in enumerate(syllubuse_links):
+            if i < 5:
+                link.click()
+                sleep(5)
+                # ここまで来たらデータをsoupから取得できる
+                data = driver.page_source.encode('utf-8')
+                soup = BeautifulSoup(data, "html.parser")
+                #print(soup.title)
+                #driver.save_screenshot(f"test{i}.png")
+            else:
+                break
+
+        driver.quit()
