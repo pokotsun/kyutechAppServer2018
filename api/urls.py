1207476bf8a349ebdef00aad46a6903ed64b38a8
pokotsun <gorigolila@gmail.com>
2018-04-26 00:12:52 +0900

    urlを複数形に統一

--- api/urls.py
+++ api/urls.py
@@ -11,12 +11,12 @@ app_name = "api"
 
 router = routers.DefaultRouter()
 router.register(r'news-headings', NewsHeadingViewSet)
-router.register(r'syllabus', SyllabusViewSet)
-router.register(r'user', UserViewSet)
+router.register(r'syllabuses', SyllabusViewSet)
+router.register(r'users', UserViewSet)
 #router.register(r'news', NewsViewSet)
 
 urlpatterns = [
     #path(r'news/code:<int:code>', FilteredNewsViewSet.as_view()),
     path(r'news/code-<int:code>/', FilteredNewsViewSet.as_view()),
-    path(r'syllabus/day-<str:day>/period-<str:period>', FilteredSyllabusViewSet.as_view())
+    path(r'syllabuses/day-<str:day>/period-<str:period>', FilteredSyllabusViewSet.as_view())
 ]
