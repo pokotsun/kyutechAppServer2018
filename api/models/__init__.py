2ea719e0cf91e8d11bbbbb502a7b83a73d59e7dc
pokotsun <gorigolila@gmail.com>
2018-04-25 21:41:06 +0900

    ユーザーモデルの生成

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,3 +1,4 @@
 from api.models.news import News
 from api.models.news_heading import NewsHeading
 from api.models.syllabus import Syllabus
+from api.models.user import User
---
+++ api/models/user.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# ユーザーモデル
+class User(models.Model):
+    class Meta:
+        verbose_name_plural = 'Users'
+
+    school_year = models.IntegerField(default=1)
+    department = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
