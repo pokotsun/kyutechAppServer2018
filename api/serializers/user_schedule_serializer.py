eaf1de8048d5669bdd6a85aff55335d8ae21751f
pokotsun <gorigolila@gmail.com>
2018-04-28 14:46:33 +0900

    UserScheduleのPOSTにおいてpkだけで値をsetできるようにした

--- api/serializers/user_schedule_serializer.py
+++ api/serializers/user_schedule_serializer.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 
-from ..models import UserSchedule
+from ..models import UserSchedule, User, Syllabus
 from ..serializers.syllabus_serializer import SyllabusSerializer
 from ..serializers.user_serializer import UserSerializer
 
 class UserScheduleSerializer(serializers.ModelSerializer):
     # user = UserSerializer()
     # syllabus = SyllabusSerializer()
+    user_id = serializers.PrimaryKeyRelatedField(source='user',  queryset=User.objects.all(), write_only=True)
+    user = UserSerializer(read_only=True)
+    syllabus_id = serializers.PrimaryKeyRelatedField(source='syllabus', queryset=Syllabus.objects.all(), write_only=True)
+    syllabus = SyllabusSerializer(read_only=True)
+    # is_valid = serializers.BooleanField(write_only=True)
 
     class Meta:
         model = UserSchedule
-        exclude = ('created_at', 'updated_at') # created_atのみ除く
+        fields = ('user_id', 'user', 'syllabus_id', 'syllabus', 'day', 'period', 'quarter', 'is_valid')
+        # exclude = ('created_at', 'updated_at') # created_atのみ除く
+
+    def get_user(self, obj):
+        return UserSerializer()
+
+    def get_syllabus(self, obj):
+        return SyllabusSerializer(obj.syllabus)
