7b2eeebad37e4ab6a1a3c2d94bd0b83819fb67f6
Atsushi KONISHI <crossover.ak@outlook.jp>
2018-10-14 04:13:02 +0900

    implemented user serializer create, update

--- api/serializers/user_serializer.py
+++ api/serializers/user_serializer.py
@@ -1,9 +1,31 @@
 from rest_framework import serializers
 
 from ..models.user import User
+from ..models.school_year import SchoolYear
+from ..models.department import Department 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ('__all__')
+
+    email = serializers.EmailField()
+    content = serializers.CharField(max_length=200)
+    created = serializers.DateTimeField()
+
+    def update(self, instance, validated_data):
+
+        instance.school_year = SchoolYear.objects.get(unique_code=validated_data["school_year"])
+        instance.department = Department.objects.get(unique_code=validated_data["department"])
+        instance.save()
+        return instance
+
+    def create(self, validated_data):
+        new_user = User(
+            school_year = SchoolYear.objects.get(unique_code=validated_data["school_year"]),
+            department = Department.objects.get(unique_code=validated_data["department"])
+        )
+        
+        new_user.save()
+        return new_user
