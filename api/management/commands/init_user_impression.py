f833a96dbb371d716882064f1b094cf33632008f
pokotsun <gorigolila@gmail.com>
2019-05-15 23:10:04 +0900

    rename spread_sheat_test file

--- api/management/commands/init_user_impression.py
+++ api/management/commands/init_user_impression.py
@@ -8,10 +8,10 @@ from datetime import datetime as dt
 class Command(BaseCommand):
 
     # python manage.py helpで表示されるメッセージ
-    help = 'departmentに関するDBの初期化を行う'
+    help = 'userImpressionに関するDBの初期化を行う'
 
     # コマンドライン引数を指定する(argparseモジュール)
-    # 今回はblog_idという名前で取得する
+    # この例ではblog_idという名前で取得する
     # def add_arguments(self, parser):
     #     parser.add_argument('blog_id', nargs='+', type=int)
 
@@ -35,6 +35,4 @@ class Command(BaseCommand):
                 request_pd = row[4]
             )
             print(impression)
-
-# wks.update_acell('A1', 'Hello World!')
-# print(wks.acell('A1'))
+            # impression.save()
